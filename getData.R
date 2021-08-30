d2cc039e68a9b9ed4902a14822607bf9c9d383a0
Jonathan Hobbs <51300209+jph314@users.noreply.github.com>
2021-08-30 10:33:57 +0100

    Add files via upload

--- getData.R
+++ getData.R
@@ -1,85 +1,40 @@
-# ---
-# Get and clean data for the app
-# Yannis Galanakis; <i.galanakis@kent.ac.uk>
-# Mar 2021
-#---
-
-# Packages ----
-packages <- c('tidyverse', 'naniar', 'haven', 'survey',
-              'data.table', 'lubridate', 'ggalt', 'cowplot','animation',
-              'patchwork', 'sp', 'scales', 'raster', 'rgeos', 'mapproj',
-              'rgdal', 'maptools', 'emojifont', 'nord', 'paletteer', 'plotly', 'tibble')
-pkg_notinstall <-  packages[!(packages %in% installed.packages()[,"Package"])]
-
-lapply(pkg_notinstall, install.packages, dependencies = TRUE)
-lapply(packages, library, character.only = TRUE)
-if (!require(gpclib)) install.packages("gpclib", type="source");library(gpclib)
-gpclibPermit()  # Gives maptool permisssion to use gpclib
-load.fontawesome()
-
 # Download data ----
-# Create a temp. file
+# Create a temp. file.
 temp <- tempfile()
-# Use `download.file()` to fetch the file into the temp. file
+# Use `download.file()` to fetch the file into the temp. file.
 download.file("http://download.companieshouse.gov.uk/BasicCompanyDataAsOneFile-2021-08-01.zip",temp)
-# Use unz() to extract the target file from temp. file
-repD<- read_csv(unz(temp, "BasicCompanyDataAsOneFile-2021-08-01.csv"))
+# Use unz() to extract the target file from temp. file.
+repD <- fread(unz(temp, "BasicCompanyDataAsOneFile-2021-08-01.csv"))
 # Remove the temp file via 'unlink()'
 unlink(temp)
 # Make incorporation date as date format.
-repD$IncorporationDate <- as.Date(repD$IncorporationDate, "%d/%m/%Y")
-# keep since 2019
-Total192021 <- repD[which(repD$IncorporationDate >= "2019-01-01" &
-                            repD$IncorporationDate <= "2021-07-31"), ]
-Total192021 <- Total192021[c(2,7,9:15,23:30)]
-Total192021 <- Total192021[-c(8,10:13)]
-write.csv(Total192021, "data/registerSince2019.csv", row.names=F)
-### Make usable dataframe to the app
-# delete unnecessary columns
-register <- Total192021[-c(2:3, 5:6)]
+repD <- repD %>% rename(date=IncorporationDate)
+repD$date <- as.Date(repD$date, "%d/%m/%Y")
+# Keep records since 2019.
+Total192021 <- repD[which(repD$date >= "2019-01-01" &
+                            repD$date <= "2021-07-31"), ]
+### Make usable dataframe for the app.
+# Delete unnecessary columns.
+register <- repD[which(repD$date >= "2019-01-01" &
+                            repD$date <= "2021-07-31"), c(2,10,15,27)]
 
-# SIC codes
-# Keep only the code from SIC
-register$SIC5dg1 <- as.numeric(gsub("([0-9]+).*$", "\\1", register$SICCode.SicText_1)) # pattern is by finding a set of numbers in the start and capturing them
-register$Class   <- register$SIC5dg1/10
-register$SIC5dg1 <- as.integer(register$SIC5dg1)
-register$Class   <- as.integer(register$Class)
-convert <- read.csv('C:/Users/ygala/Dropbox-UKC/Dropbox/FirmsCovid_YG/ReportMarch2021/data/sic2007conversion.csv')
-convert <- convert[-c(1)]
-# add abbreviation 
-abbreviation <- read.csv("data/SectionAbbreviation.csv")
-convert <- merge(convert, abbreviation, by="Section")
-convert1<-convert[-c(9:10)]
-write.csv(convert1, "data/convert.csv", row.names=F)
+# SIC codes ----
+# Keep only the code from SIC.
+register$SIC5dg1 <- as.integer(gsub("([0-9]+).*$", "\\1", register$SICCode.SicText_1)) # pattern is by finding a set of numbers in the start and capturing them
+register$Class <- as.integer(register$SIC5dg1/10)
+# Overseas companies are not supplied with SIC codes. They are therefore lost in the next step.
+# Read SIC conversion file and merge to obtain Section, Division, Group and Class.
+convert1 <- fread("data/convertSIC.csv")
 register <- merge(register, convert1, by="Class")
-#register <- register[-c(21:22)]
-# Remove duplicates based on week columns
-register <- register %>%
-  distinct(Class, IncorporationDate, CompanyNumber, .keep_all = TRUE)
-write.csv(register, "data/registerSectorsUK.csv", row.names=F)
 
-# make one file with PC, Areas, Country
-areaPC <- read.csv("data/Postcodes summaryCLEAN.csv")
-# add countries
-postcod2country <- read.csv("data/convertedPC2country.csv")
-postcod2country <- postcod2country[-c(1)]
-postcod2country <- postcod2country %>% rename (PostcodeArea= Postcode.area)
-PCcountryArea <- merge(areaPC[ , c("PostcodeArea","Area.covered")],postcod2country,by = "PostcodeArea")
-PC2regions <- read_csv("data/PostcodeArea2Regions.csv")
-PCcountryArea <- merge(PCcountryArea, PC2regions, by="PostcodeArea")
-#save it
-write.csv(PCcountryArea, "data/regionsUK.csv", row.names = F)
-# by Country
-# keep only the first 1 or 2 letters before the numbers in the Postcode
-register <- register %>% rename(postcode = RegAddress.PostCode)
-register$postcodeArea <- sub("^([[:alpha:]]*).*", "\\1", register$postcode)
-PCcountryArea <- PCcountryArea %>% rename (postcodeArea = PostcodeArea)
-registerPC <- merge(register, PCcountryArea, by="postcodeArea")
-registerPC <- registerPC %>%
-  distinct(postcodeArea, IncorporationDate, CompanyNumber,
-           Area.covered, Region, Country, .keep_all = TRUE)
-registerPC <- registerPC[-c(5, 8:10)]
-write.csv(registerPC, "data/registerSectorsRegionsUK.csv", row.names = F)
+# NUTS2 ----
+#Keep only postcode district
+register$postcodeDistrict <- gsub("[ .]", "", register$RegAddress.PostCode)
+register$postcodeDistrict <- sub("...$", "", register$postcodeDistrict)
+#Convert PC district to NUTS2
+pcd2NUTS <- fread("data/convertNUTS.csv")
+registerPC <- merge(register, pcd2NUTS, by="postcodeDistrict")
+#Save full register ready for use.
+fwrite(registerPC, "data/registerSectorsRegionsUK.csv", row.names = F)
 
-library(fst)
-write_fst(registerPC, "data/registerSectorsRegionsUK.fst", 100)
+# registerPC <- fread("data/registerSectorsRegionsUK.csv")
